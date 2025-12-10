1227d601c02770486acafc11b126984ac375e186
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-10 22:37:34 +0530

    Loops

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#!bin/bash
+
+#CODE started 
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/roboshop"
+SCRIPT_NAME=$(echo $0| cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "SCRIPT started executing $(date)" | tee -a $LOG_FILE 
+
+USERID=$(id -u)
+
+date &>>$LOG_FILE
+
+if [ $USERID -ne 0 ] ; then
+echo " Please run with root user else will not work"
+exit 1
+fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ] ;  then 
+         echo -e "$2... $R failure $N"
+        exit 1
+    else
+         echo "$2... $G success $N"
+
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>LOG_FILE
+VALIDATE $? "Installing mongo db"
+
+systemctl enable mongod 
+VALIDATE $? "enable mongodB"
+
+
+systemctl start mongod 
