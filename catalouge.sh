28471b48c540e673cc09b9d91fa8d617c2d8671c
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-12 21:35:18 +0530

    mongodb

--- catalouge.sh
+++ catalouge.sh
@@ -15,6 +15,7 @@ MONGODB_HOST="mongodb.daws8s.shop"
 LOGS_FOLDER="/var/log/roboshop"
 SCRIPT_NAME=$(echo $0| cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "SCRIPT started executing $(date)" | tee -a $LOG_FILE 
@@ -46,8 +47,15 @@ VALIDATE $? "enable node js"
 
 dnf install nodejs -y &>>LOG_FILE
 
+id=roboshop
+
+if [ $? -ne 0] then
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOG_FILE
 VALIDATE $? "useradd"
+
+echo "already user add"
+else 
+
 mkdir /app 
 VALIDATE $? "app directory creation"
 
@@ -60,12 +68,12 @@ unzip /tmp/catalogue.zip &>>LOG_FILE
 cd /app 
 npm install &>>LOG_FILE
 VALIDATE $? "npm install"
-cp catalogue.service /etc/systemd/system/catalouge.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalouge.service
 VALIDATE $? "copying catalogue service "
 
 systemctl daemon-reload
 VALIDATE $? " demon reaload"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 
 VALIDATE $? " copying mongo repo"
 dnf install mongodb-mongosh -y
