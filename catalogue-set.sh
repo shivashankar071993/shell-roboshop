c1ea9329ed9ce90d5740e3bb2d9f26d4df782f08
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-12 23:48:09 +0530

    mongodb

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -43,7 +43,7 @@ dnf install nodejs -y &>>$LOG_FILE
 
 id=roboshop
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOG_FILE
   
 else 
