f00ebde2f52a5ebf14bfc1b0d6223352b24a37be
shivashankar071993 <shivashankar071993@gmail.com>
2025-12-15 18:01:46 +0530

    mongodb

--- redis.sh
+++ redis.sh
@@ -44,24 +44,24 @@ VALIDATE(){
 
 dnf module disable redis -y &>>$LOG_FILE
 
-VALIDATE $? "disabling the redis" >&&$LOG_FILE
+VALIDATE $? "disabling the redis" &>>$LOG_FILE
 
-dnf module enable redis:7 -y >&&$LOG_FILE
+dnf module enable redis:7 -y &>>$LOG_FILE
 
 VALIDATE $? "enabling the redis 7"
 
-dnf install redis -y >&&$LOG_FILE
+dnf install redis -y &>>$LOG_FILE
 
 VALIDATE $? "install redis y"
 
 sed -i -e '/127.0.0.0/0.0.0.0/g' etc/redis/redis.conf c protected-mode no /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections to redis" 
 
-systemctl enable redis >&&$LOG_FILE
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "enabling redis" 
 
 
-systemctl start redis >&&$LOG_FILE
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "starting redis" 
 
 End_time=$(date +%s)
